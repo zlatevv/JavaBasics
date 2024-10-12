45339b74abb121bc6717f686334f6a30699624b2
zlatev <146636754+zlatevv@users.noreply.github.com>
2024-10-12 12:19:38 +0300

    Delete greetingByName.java

--- greetingByName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
- class greetingByName {
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         String name = scanner.nextLine();
-         System.out.println("Hello, " + name + "!");
-     }
-}
