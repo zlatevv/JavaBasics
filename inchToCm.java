41f459dedc3db42e76a4ad0daf2cabc59216c58a
zlatev <146636754+zlatevv@users.noreply.github.com>
2024-10-12 12:19:53 +0300

    Delete inchToCm.java

--- inchToCm.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class inchToCm {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double inches = Double.parseDouble(scanner.nextLine());
-        double cm = inches * 2.54;
-        System.out.print(cm);
-    }
-}
