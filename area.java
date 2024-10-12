6ee1ce0f0e242303f01f448df0ed0f9fe9a366ff
zlatev <146636754+zlatevv@users.noreply.github.com>
2024-10-12 12:18:47 +0300

    Delete area.java

--- area.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class area {
-    public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        int width = Integer.parseInt(a.nextLine());
-        int length = Integer.parseInt(a.nextLine());
-        System.out.println(width * length);
-    }
-}
